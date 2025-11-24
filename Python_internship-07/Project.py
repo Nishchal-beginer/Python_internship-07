376db4cc3401b85085919cc88d018654db431261
Nishchal Khajuria <nischalkhajuria.nk@gmail.com>
2025-11-24 21:58:15 +0530

    Update Project.py

--- Python_internship-07/Project.py
+++ Python_internship-07/Project.py
@@ -2,13 +2,7 @@ import os
 from PIL import Image
 
 def batch_resize_images(input_folder, output_folder, size=(800, 800)):
-    """
-    Resizes all images in the input folder and saves them to the output folder.
-
-    :param input_folder: Path to folder containing images
-    :param output_folder: Path where resized images will be saved
-    :param size: Tuple (width, height)
-    """
+   
 
     # Create output folder if needed
     if not os.path.exists(output_folder):
@@ -31,11 +25,9 @@ def batch_resize_images(input_folder, output_folder, size=(800, 800)):
 
     print("Batch resizing complete!")
 
-# -----------------------------------
-# ✅ CALL THE FUNCTION *HERE* (outside)
-# -----------------------------------
 batch_resize_images(
     r"d:\Internships\Python internship\Python_internship-07\input_folder",
     r"d:\Internships\Python internship\Python_internship-07\output_folder",
     size=(800, 800)
 )
+
